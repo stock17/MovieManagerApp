682ed99edd0e9954fe372de407d86380fa96f9d7
Yury <31337@mail.ru>
2017-03-30 19:22:55 +0400

    Add tests

--- app/src/androidTest/java/com/example/yury/moviemanagerapp/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/example/yury/moviemanagerapp/ExampleInstrumentedTest.java
@@ -1,12 +1,21 @@
 package com.example.yury.moviemanagerapp;
 
+import android.app.Activity;
 import android.content.Context;
 import android.support.test.InstrumentationRegistry;
+import android.support.test.espresso.contrib.RecyclerViewActions;
+import android.support.test.rule.ActivityTestRule;
 import android.support.test.runner.AndroidJUnit4;
 
+import com.example.yury.moviemanagerapp.activities.MainActivity;
+
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import static android.support.test.espresso.Espresso.onView;
+import static android.support.test.espresso.action.ViewActions.click;
+import static android.support.test.espresso.matcher.ViewMatchers.withId;
 import static org.junit.Assert.*;
 
 /**
@@ -23,4 +32,22 @@ public class ExampleInstrumentedTest {
 
         assertEquals("com.example.yury.moviemanagerapp", appContext.getPackageName());
     }
+
+    @Rule
+    public ActivityTestRule<MainActivity> mActivityTestRule = new ActivityTestRule<MainActivity>(
+            MainActivity.class, true, true ) {};
+
+    @Test
+    public void validateRecyclerViewClickThirdElement() {
+        onView(withId(R.id.rvMovies)).perform(RecyclerViewActions.actionOnItemAtPosition(3, click()));
+
+    }
+
+
+    @Test
+    public void validateRecyclerViewClickSecondElement() {
+        onView(withId(R.id.rvMovies)).perform(RecyclerViewActions.actionOnItemAtPosition(2, click()));
+
+    }
+
 }
